58fe2fa1bc3f84235f558072645f8e1489a32cca
Dominique Brandl <brandl.dominique@gmail.com>
2019-06-03 20:05:18 +0200

    Initial commit

---
+++ PlayerInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Player))]
+public class PlayerInput : MonoBehaviour
+{
+    private Player _player;
+
+    //void Start()
+    //{
+    //    _player = GetComponent<Player>();
+    //}
+
+    //void Update()
+    //{
+    //    Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    //    _player.SetDirectionalInput(directionalInput);
+
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //        _player.OnJumpInputDown();
+    //}
+}
